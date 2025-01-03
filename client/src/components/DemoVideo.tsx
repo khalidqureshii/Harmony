fa0f19d02f3396ada1de99a652d5accb585e9c45
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2025-01-03 16:04:47 +0530

    Demo Video Functionality Added

--- client/src/components/DemoVideo.tsx
+++ client/src/components/DemoVideo.tsx
@@ -1,15 +1,6 @@
 const DemoVideo = () => {
   return (
-    <div><iframe
-    className="w-full h-[315px]"
-    src="https://www.youtube.com/embed/vflWLItnwfA?si=ifZysP5TeNp2Kpx6"
-    title="YouTube video player"
-    frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    referrerPolicy="strict-origin-when-cross-origin"
-    allowFullScreen
-></iframe>
-
+    <div>
 <iframe className="w-full h-[315px]" src="https://www.youtube.com/embed/wQn-Yfl2p-c?si=iLpHQj42KgmL5TIr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 </div>
   )
