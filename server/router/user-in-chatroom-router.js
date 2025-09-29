3c02b38d332c37c423ad0d315ebad3bf53375337
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2025-09-30 00:13:37 +0530

    Fixed wrong import

--- server/router/user-in-chatroom-router.js
+++ server/router/user-in-chatroom-router.js
@@ -1,10 +1,8 @@
 import express from "express";
 const router = express.Router();
-import userInChatroomSchema from "../validators/user-in-chatroom-validator.js";
-import validateUserInChatroom from "../middlewares/user-in-chatroom-middleware.js";
 import * as userInChatroomControllers from "../controller/user-in-chatroom-controller.js";
 
-router.route("/addUserInChatroom").post(validateUserInChatroom(userInChatroomSchema), userInChatroomControllers.addUserInChatroom);
+router.route("/addUserInChatroom").post(userInChatroomControllers.addUserInChatroom);
 router.route("/removeUserFromChatroom").post(removeUserFromChatroom);
 router.route("/requestForChatroom").post(requestForChatroom);
 
