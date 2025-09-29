dccb9ee6d42a2c2b2c50ed4ff7eff61b6f81ee98
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2025-09-30 00:16:45 +0530

    Fixed user-in-chatroom router

--- server/router/user-in-chatroom-router.js
+++ server/router/user-in-chatroom-router.js
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 import * as userInChatroomControllers from "../controller/user-in-chatroom-controller.js";
 
-router.route("/addUserInChatroom").post(userInChatroomControllers.addUserInChatroom);
-router.route("/removeUserFromChatroom").post(removeUserFromChatroom);
-router.route("/requestForChatroom").post(requestForChatroom);
+router.route("/approveUserInChatroom").post(userInChatroomControllers.approveUserInChatroom);
+router.route("/removeUserFromChatroom").post(userInChatroomControllers.removeUserFromChatroom);
+router.route("/requestForChatroom").post(userInChatroomControllers.requestForChatroom);
 
 export default router;
