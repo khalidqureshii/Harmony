5cb2bae4006da0d81fc4e03cdf8500731d529573
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2025-09-29 12:29:12 +0530

    Fixed logo text

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header () {
     return <header className="flex flex-row justify-between customHeader py-4">
         <div className="ml-5 md:ml-7 flex flex-row items-center">
             <img src="logo.png" style={{ width: "3rem", height: "auto", objectFit: "contain" }} />
-            <button onClick={()=>{navigate("/")}}><h1 className="text-2xl ml-3 md:text-3xl ">Chime</h1></button>
+            <button onClick={()=>{navigate("/")}}><h1 className="text-2xl ml-3 md:text-3xl text-black text-center font-logo font-bold">Chime</h1></button>
         </div>
 
         <button onClick={()=>{navigate("/logout")}}><h1 className="mr-5 md:mr-7 text-xl">Logout</h1></button>
